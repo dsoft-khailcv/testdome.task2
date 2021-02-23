51343ee83ad18c490eada07ff6edd9c3857ecafc
khailcv <khailcv@d-soft.com.vn>
2021-02-23 17:37:28 +0700

    Fix result, return pair(index) instead of pair(element)

--- ex2.2.cpp
+++ ex2.2.cpp
@@ -7,29 +7,29 @@
 std::pair<int, int> findTwoSum(const std::vector<int> &list, int sum)
 {
     std::vector<int> tmp = list;
+    std::vector<int> result_tmp = list;
     std::pair<int, int> result;
     std::sort(tmp.begin(), tmp.end());
-    std::vector<int>::iterator it_begin = tmp.begin();
-    std::vector<int>::iterator it_end = tmp.end();
-
-    int tmp_sum = *it_begin + *it_end;
-    while (tmp_sum != sum && it_begin != it_end)
+    std::vector<int>::iterator it_left = tmp.begin();
+    std::vector<int>::iterator it_right = tmp.end();
+    int tmp_sum = *it_left + *it_right;
+    while (tmp_sum != sum && it_left != it_right)
     {
         if (tmp_sum > sum)
         {
-            it_end--;
-            tmp_sum = *it_begin + *it_end;
+            it_right--;
+            tmp_sum = *it_left + *it_right;
         }
         else
         {
-            it_begin++;
-            tmp_sum = *it_begin + *it_end;
+            it_left++;
+            tmp_sum = *it_left + *it_right;
         }
     }
-    if ((it_begin != it_end) && (tmp_sum == sum))
+    if ((it_left != it_right) && (tmp_sum == sum))
     {
-        result.first = *it_begin;
-        result.second = *it_end;
+        result.first = find (result_tmp.begin(), result_tmp.end(), *it_left) - result_tmp.begin();      
+        result.second = find (result_tmp.begin()+1, result_tmp.end(), *it_right) - result_tmp.begin();
     }
     else
     {
@@ -44,7 +44,7 @@ std::pair<int, int> findTwoSum(const std::vector<int> &list, int sum)
 int main()
 {
     std::vector<int> list = {3, 1, 5, 7, 5, 9};
-    std::pair<int, int> indices = findTwoSum(list, 8);
+    std::pair<int, int> indices = findTwoSum(list, 10);
     std::cout << indices.first << '\n' << indices.second;
 }
 #endif
