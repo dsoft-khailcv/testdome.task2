90e10b1446148d3b169e7ba886e791130bc7ca9b
khailcv <khailcv@d-soft.com.vn>
2021-02-24 11:09:05 +0700

    Edit algorithm find result pair

--- ex2.2.cpp
+++ ex2.2.cpp
@@ -12,7 +12,7 @@ std::pair<int, int> findTwoSum(const std::vector<int> &list, int sum)
     std::sort(tmp.begin(), tmp.end());
     std::vector<int>::iterator it_left = tmp.begin();
     std::vector<int>::iterator it_right = tmp.end() - 1;
-    
+
     int tmp_sum = *it_left + *it_right;
     while (tmp_sum != sum && it_left != it_right)
     {
@@ -29,8 +29,9 @@ std::pair<int, int> findTwoSum(const std::vector<int> &list, int sum)
     }
     if ((it_left != it_right) && (tmp_sum == sum))
     {
-        result.first = find (result_tmp.begin(), result_tmp.end(), *it_left) - result_tmp.begin();      
-        result.second = find (result_tmp.begin()+1, result_tmp.end(), *it_right) - result_tmp.begin();
+        std::vector<int>::iterator res_first_it = find(result_tmp.begin(), result_tmp.end(), *it_left);
+        result.first = res_first_it - result_tmp.begin();
+        result.second = find(res_first_it, result_tmp.end(), *it_right) - result_tmp.begin();
     }
     else
     {
@@ -44,8 +45,9 @@ std::pair<int, int> findTwoSum(const std::vector<int> &list, int sum)
 #ifndef RunTests
 int main()
 {
-    std::vector<int> list = {3, 1, 5, 7, 5, 9, 3, 1, 5, 7, 5, 9};
-    std::pair<int, int> indices = findTwoSum(list, 10);
-    std::cout << indices.first << '\n' << indices.second;
+    std::vector<int> list = {3, 1, 5, 7, 5, 9, 3, 1, 5,734, 42, 65, 56, 76,12, 43, 7, 5, 9, 43, 65, 654, 675, 75, 35, 57, 3};
+    std::pair<int, int> indices = findTwoSum(list, 6);
+    std::cout << indices.first << '\n'
+              << indices.second;
 }
 #endif
